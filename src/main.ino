f4bdd62280c84e0a630a5e2622ab468faf423cd7
Tony Rieker <tony@plenary.org>
2018-10-27 01:29:14 -0700

    source control

---
+++ src/main.ino
@@ -0,0 +1,107 @@
+// Transmitter Test. Transmitter is on the desk and stationary
+
+// LoRa 9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example LoRa9x_RX
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 8 // GO / IRQ
+
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+void setup()
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  // uncomment if you want to wait for serial to attach
+  //while (!Serial);
+  Serial.begin(9600);
+  delay(100);
+
+  Serial.println("Arduino LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  // http://www.airspayce.com/mikem/arduino/RadioHead/classRH__RF95.html#ab9605810c11c025758ea91b2813666e3
+  rf95.setModemConfig(RH_RF95::Bw31_25Cr48Sf512);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void loop()
+{
+  //Serial.println("\nSending to rf95_server");
+  // Send a message to rf95_server
+
+  char radiopacket[20] = "Hello World #      ";
+  itoa(packetnum++, radiopacket+13, 10);
+  //Serial.print("Sending "); Serial.println(radiopacket);
+  radiopacket[19] = 0;
+
+  //Serial.println("Sending..."); delay(10);
+  rf95.send((uint8_t *)radiopacket, 20);
+
+  //Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  //Serial.println("Waiting for reply...");
+  if (rf95.waitAvailableTimeout(2000))
+  {
+    // Should be a reply message for us now
+    if (rf95.recv(buf, &len))
+   {
+      //Serial.print("Got reply: ");
+      //Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+  else
+  {
+    //Serial.println("No reply, is there a listener around?");
+  }
+  delay(5000);
+}
